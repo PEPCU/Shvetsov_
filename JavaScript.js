26f0d07f9da1d18f68b5dc590881c650a570865f
PEPCU <147733464+PEPCU@users.noreply.github.com>
2024-01-22 20:39:02 +0500

    Delete JavaScript.js

--- JavaScript.js
+++
@@ -1,7 +0,0 @@
-const boxes = document.querySelectorAll('.tabtab');
-if (boxes.length) {
-   boxes.forEach(box => box.addEventListener('click', myBoxClickHandler));
-} else {
-  console.log('These aren\'t the boxes you\'re looking for.');
-   // на самом деле вся ветка 'else' не нужна
-}
